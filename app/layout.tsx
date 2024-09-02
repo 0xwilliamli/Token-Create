89d2d7eb84093f250a6c015c15b391a836cdbad8
William <williamli.elite@gmail.com>
2024-09-03 04:06:46 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { cookies } from 'next/headers';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Next Shadcn",
+  title: "Token Generate Platform",
 };
 
 export default async function RootLayout({
